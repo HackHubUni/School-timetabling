f1f4f8ae232cbfc246e9ef5b5ca72a6f900238a8
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-04-17 15:55:26 -0400

    refactor code put solver in solver.py

--- Linear_Programming/main.py
+++ Linear_Programming/main.py
@@ -1,121 +1,26 @@
-from ortools.sat.python import cp_model
-from Linear_Programming.utils import Calendar
 
 
+from Linear_Programming.solver import solver
 def main():
-  # Crear el modelo
-  model = cp_model.CpModel()
+  subjects_name_list = ["Programación", "AlgrebraCP", "AlgebraConf"]  # "AlgrebraCP", "Programación"]
+  dict_subjects_by_time = {"Programación": 2, "AlgrebraCP": 2, "AlgebraConf": 1}
+  teachers_names = ["Piad", "Paco", "Celia"]  # "Paco", "Celia"]
+  classrooms_names = ["1", "2", "3", "Postgrado"]
+  groups_names = ["C111", "C112", "cvvv"]
+  dict_group_subject_time = {}
+  for item in groups_names:
+    dict_group_subject_time[item] = dict_subjects_by_time
 
-  asignaturas = ["Programación", "AlgrebraCP","AlgebraConf" ]  # "AlgrebraCP", "Programación"]
-  asignaturas_tiempo = {"Programación": 2, "AlgrebraCP": 2,"AlgebraConf":1}
-  profesores = ["Piad","Paco","Celia" ]  # "Paco", "Celia"]
-  aulas = ["1", "2", "3", "Postgrado"]
-  grupos = ["C111", "C112", "cvvv"]
-  grupo_asignatura_tiempo = {}
-  for item in grupos:
-    grupo_asignatura_tiempo[item] = asignaturas_tiempo
+  shifts = [1, 2, 3]
+  days = [1, 2]
 
-  turnos = [1, 2,3]
-  dias = [1, 2]
-
-  asignaturas_profes = {
+  dict_teachers_to_subjects = {
     "Piad": ["Programación", "AlgrebraCP"],
-     "Celia": ["AlgebraConf"],
-     "Paco": ["AlgrebraCP"],
+    "Celia": ["AlgebraConf"],
+    "Paco": ["AlgrebraCP"],
 
   }
-
-  asignatura_list_profes = {
-    "Programación": ["Piad"],
-     "AlgebraConf": ["Celia"],
-    "AlgrebraCP": ["Piad","Paco"]
-
-  }
-
-  calendar = Calendar(asignaturas, profesores, asignatura_list_profes, grupo_asignatura_tiempo, len(dias), len(turnos),
-                      aulas, grupos)
-
-  vars = {}
-  for dia in dias:
-    for turno in turnos:
-      for grupo in grupos:
-        for asignatura in asignaturas:
-          for aula in aulas:
-            for profesor in profesores:
-              vars[profesor, asignatura, aula, grupo, turno, dia] = model.NewBoolVar(
-                f'profesor:{profesor},asignatura:{asignatura},aula:{aula},grupo:{grupo},turno:{turno},dia:{dia}')
-
-  # Que siempre se den todos los turnos de las asignaturas en cada grupo
-  for asignatura in asignaturas:
-    for grupo in grupos:
-      s = sum(
-        vars[profesor, asignatura, aula, grupo, turno, dia] for profesor in profesores for aula in aulas
-        for turno in turnos for dia in dias if asignatura in asignaturas_profes[profesor])
-      model.add(s == asignaturas_tiempo[asignatura])
-
-  # Que un grupo pueda estar en un turno en una sola aula
-  for dia in dias:
-    for turno in turnos:
-      for grupo in grupos:
-        s = sum(vars[profesor, asignatura, aula, grupo, turno, dia] for profesor in profesores
-                for asignatura in asignaturas
-                for aula in aulas
-                if asignatura in asignaturas_profes[profesor])
-        model.add(s <= 1)
-
-  # Solo se puede asignar a un aula una materia un dia x
-  for aula in aulas:
-    for dia in dias:
-      for turno in turnos:
-        s = sum(
-          vars[profesor, asignatura, aula, grupo, turno, dia] for grupo in grupos for profesor in profesores
-          for asignatura in asignaturas if asignatura in asignaturas_profes[profesor])
-        model.add(s <= 1)
-
-    # Que un profesor puede estar en un aula a la vez
-
-    for dia in dias:
-      for turno in turnos:
-        for profesor in profesores:
-          for grupo in grupos: # se pone los grupos porque no importa si un profe en un turno en una aula le imparte a mas de un grupo
-            s = sum(
-              vars[profesor, asignatura, aula, grupo, turno, dia]  for aula in aulas
-              for asignatura in asignaturas if asignatura in asignaturas_profes[profesor])
-            model.add(s <= 1)
-
-  # Crear el solucionador y resolver
-  solver = cp_model.CpSolver()
-  status = solver.Solve(model)
-
-  if status == cp_model.INFEASIBLE:
-    print("No se puede resolver")
-    # Analizar el estado del modelo
-    print("Código de estado:", solver.status_name())
-    print("Número de nodos explorados:", solver.NumBranches())
-    print("Tiempo de ejecución:", solver.WallTime())
-    return
-
-  if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    for profesor in profesores:
-      for asignatura in asignaturas_profes[profesor]:
-        for aula in aulas:
-          for grupo in grupos:
-            for turno in turnos:
-              for dia in dias:
-                if solver.value(vars[profesor, asignatura, aula, grupo, turno, dia]) == 1:
-                  to_print = f'profesor:{profesor},asignatura:{asignatura},aula:{aula},grupo:{grupo},turno:{turno},dia:{dia}'
-                  print(to_print)
-                  if False:
-                    calendar.add(grupo, aula, profesor, str(dia), str(turno), asignatura)
-                  else:
-                      try:
-                        calendar.add(grupo, aula, profesor, str(dia), str(turno), asignatura)
-                      except Exception as e:
-                        raise Exception(f'En {to_print} se lanzo el error: \n {e}')
-
-  # Chequea que se cumple la cantidad de horas clases por grupo de alas asignaturas por semana
-  calendar.finish()
-
+  solver(subjects_name_list,dict_subjects_by_time,teachers_names,classrooms_names,groups_names,dict_group_subject_time,shifts,days,dict_teachers_to_subjects)
 
 if __name__ == "__main__":
   main()
---
+++ Linear_Programming/solver.py
@@ -0,0 +1,133 @@
+from ortools.constraint_solver.pywrapcp import IntVar
+from ortools.sat.python import cp_model
+from Linear_Programming.utils import Calendar
+
+
+def _get_dic_subjects_to_teachers(teachers: list[str], subjects: list[str],
+                                  dict_teachers_to_subjects: dict[str, list[str]]) -> dict[str, list[str]]:
+  answer: dict[str, list[str]] = {}
+  # Inicializar el diccionario
+  for subject_name in subjects:
+    answer[subject_name] = []
+
+  # Cada asignatura agregarle los profes
+  for teacher_name in teachers:
+    subjects_list: list[str] = dict_teachers_to_subjects[teacher_name]
+    for subject_name in subjects_list:
+      answer[subject_name].append(teacher_name)
+
+  return answer
+
+
+def solver(subjects_name_list: list[str], dict_subjects_by_time: dict[str:int], teachers_names: list[str],
+                 classrooms_names: list[str],
+                 groups_names: list[str], dict_group_subject_time: dict[str, dict[str:int]], shifts: list[int],
+                 days: list[int], dict_teachers_to_subjects: dict[str, list[str]]):
+        """
+        Generates a schedule for assigning teachers to subjects in classrooms for different groups and shifts.
+
+        Parameters:
+        - subjects_name_list (list[str]): List of subject names to be scheduled.
+        - dict_subjects_by_time (dict[str:int]): Dictionary mapping subjects to the number of times they need to be scheduled.
+        - teachers_names (list[str]): List of teacher names available for scheduling.
+        - classrooms_names (list[str]): List of classroom names available for scheduling.
+        - groups_names (list[str]): List of group names to schedule the subjects for.
+        - dict_group_subject_time (dict[str,dict[str:int]]): Nested dictionary mapping group names to subjects and the number of times each subject needs to be scheduled.
+        - shifts (list[int]): List of shift numbers available for scheduling.
+        - days (list[int]): List of day numbers available for scheduling.
+        - dict_teachers_to_subjects (dict[str,list[str]]): Dictionary mapping teacher names to the subjects they can teach.
+
+        Returns:
+        - None
+        """
+
+        # Crear el modelo
+        model = cp_model.CpModel()
+
+        dict_subjects_to_teachers = _get_dic_subjects_to_teachers(teachers_names, subjects_name_list,
+                                                                  dict_teachers_to_subjects)
+
+        calendar = Calendar(subjects_name_list, teachers_names, dict_subjects_to_teachers, dict_group_subject_time, len(days),
+                            len(shifts),
+                            classrooms_names, groups_names)
+
+        variables: dict[tuple[str, str, str, str, int, int], "IntVar"] = {}
+        for dia in days:
+            for turno in shifts:
+                for grupo in groups_names:
+                  for asignatura in subjects_name_list:
+                    for aula in classrooms_names:
+                      for profesor in teachers_names:
+                        variables[profesor, asignatura, aula, grupo, turno, dia] = model.NewBoolVar(
+                          f'profesor:{profesor},asignatura:{asignatura},aula:{aula},grupo:{grupo},turno:{turno},dia:{dia}')
+
+        # Que siempre se den todos los turnos de las asignaturas en cada grupo
+        for asignatura in subjects_name_list:
+          for grupo in groups_names:
+            s = sum(
+              variables[profesor, asignatura, aula, grupo, turno, dia] for profesor in teachers_names for aula in classrooms_names
+              for turno in shifts for dia in days if asignatura in dict_teachers_to_subjects[profesor])
+            model.add(s == dict_subjects_by_time[asignatura])
+
+        # Que un grupo pueda estar en un turno en una sola aula
+        for dia in days:
+          for turno in shifts:
+            for grupo in groups_names:
+              s = sum(variables[profesor, asignatura, aula, grupo, turno, dia] for profesor in teachers_names
+                      for asignatura in subjects_name_list
+                      for aula in classrooms_names
+                      if asignatura in dict_teachers_to_subjects[profesor])
+              model.add(s <= 1)
+
+        # Solo se puede asignar a un aula una materia un dia x
+        for aula in classrooms_names:
+          for dia in days:
+            for turno in shifts:
+              s = sum(
+                variables[profesor, asignatura, aula, grupo, turno, dia] for grupo in groups_names for profesor in teachers_names
+                for asignatura in subjects_name_list if asignatura in dict_teachers_to_subjects[profesor])
+              model.add(s <= 1)
+
+          # Que un profesor puede estar en un aula a la vez
+
+          for dia in days:
+            for turno in shifts:
+              for profesor in teachers_names:
+                for grupo in groups_names:  # se pone los grupos porque no importa si un profe en un turno en una aula le imparte a mas de un grupo
+                  s = sum(
+                    variables[profesor, asignatura, aula, grupo, turno, dia] for aula in classrooms_names
+                    for asignatura in subjects_name_list if asignatura in dict_teachers_to_subjects[profesor])
+                  model.add(s <= 1)
+
+        # Crear el solucionador y resolver
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
+
+        if status == cp_model.INFEASIBLE:
+          print("No se puede resolver")
+          # Analizar el estado del modelo
+          print("Código de estado:", solver.status_name())
+          print("Número de nodos explorados:", solver.NumBranches())
+          print("Tiempo de ejecución:", solver.WallTime())
+          return
+
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+          for profesor in teachers_names:
+            for asignatura in dict_teachers_to_subjects[profesor]:
+              for aula in classrooms_names:
+                for grupo in groups_names:
+                  for turno in shifts:
+                    for dia in days:
+                      if solver.value(variables[profesor, asignatura, aula, grupo, turno, dia]) == 1:
+                        to_print = f'profesor:{profesor},asignatura:{asignatura},aula:{aula},grupo:{grupo},turno:{turno},dia:{dia}'
+                        print(to_print)
+                        if False:
+                          calendar.add(grupo, aula, profesor, str(dia), str(turno), asignatura)
+                        else:
+                          try:
+                            calendar.add(grupo, aula, profesor, str(dia), str(turno), asignatura)
+                          except Exception as e:
+                            raise Exception(f'En {to_print} se lanzo el error: \n {e}')
+
+        # Chequea que se cumple la cantidad de horas clases por grupo de alas asignaturas por semana
+        calendar.finish()
